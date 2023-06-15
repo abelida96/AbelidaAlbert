4658cfced12ad5838cce99599e392ebc95f947e7
abelida96 <135182038+abelida96@users.noreply.github.com>
2023-06-15 17:53:43 +0800

    Add files via upload

---
+++ Abelida/Abelida/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Settings.AppStat = "Trial Version" Then
+            If My.Settings.LoadCount < 0 Then
+                Form2.ShowDialog()
+            End If
+
+            Me.Text = My.Settings.AppStat & " : Counter =>" & My.Settings.LoadCount
+            My.Settings.LoadCount -= 1
+            My.Settings.Save()
+        Else
+            Me.Text = My.Settings.AppStat
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Hello World!")
+    End Sub
+End Class
